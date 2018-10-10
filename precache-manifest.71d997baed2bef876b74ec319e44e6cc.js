af8478b1435302d2444c32b96572bee198e3c7a3
r4yng <raymondn@gmail.com>
2018-10-10 19:31:00 -0400

    Updates

---
+++ precache-manifest.71d997baed2bef876b74ec319e44e6cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-cool-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1113eb1762869f5c43d5",
+    "url": "/my-cool-app/static/js/runtime~main.1113eb17.js"
+  },
+  {
+    "revision": "033dc77c6af16ee9f349",
+    "url": "/my-cool-app/static/js/main.033dc77c.chunk.js"
+  },
+  {
+    "revision": "f5b17c1b5fdc6272f59a",
+    "url": "/my-cool-app/static/js/1.f5b17c1b.chunk.js"
+  },
+  {
+    "revision": "033dc77c6af16ee9f349",
+    "url": "/my-cool-app/static/css/main.bf5b11c4.chunk.css"
+  },
+  {
+    "revision": "1160a987bea240c0a3c284d7f2ec9805",
+    "url": "/my-cool-app/index.html"
+  }
+];
